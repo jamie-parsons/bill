1f814d8817cdd9e535e8a4cdf7e9555f9d74e3db
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-04 23:00:15 -0600

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'billboards/index'
+
+  get 'billboards/show'
+
+  get 'billboards/new'
+
+  get 'billboards/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
